80ffffd21cb00592df95a91260690a214aced520
kanutan93 <kanutan93@gmail.com>
2023-05-05 22:14:09 +0300

    init

---
+++ homework1/src/main/java/ru/msa/homework1/controller/StatusController.java
@@ -0,0 +1,15 @@
+package ru.msa.homework1.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import ru.msa.homework1.model.StatusResponse;
+
+@RestController
+public class StatusController {
+
+  @GetMapping
+  public ResponseEntity<StatusResponse> getStatus() {
+    return ResponseEntity.ok(new StatusResponse());
+  }
+}
---
+++ homework1/src/main/java/ru/msa/homework1/model/StatusResponse.java
@@ -0,0 +1,13 @@
+package ru.msa.homework1.model;
+
+public class StatusResponse {
+  private String status = "OK";
+
+  public String getStatus() {
+    return status;
+  }
+
+  public void setStatus(String status) {
+    this.status = status;
+  }
+}
